7e985b841d94e440dd87c5f4a89a19412e9c59ae
Rubén Segarra <Rubn Segarra>
2015-11-26 20:23:58 +0100

    Finished game one

---
+++ KataRubenToni/src/GameOne/Color.java
@@ -0,0 +1,25 @@
+package GameOne;
+
+/**
+ * Created by Ruben on 26/11/2015.
+ */
+public class Color {
+    private String name;
+
+    public Color(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
