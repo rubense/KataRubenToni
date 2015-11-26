431e2646a33909f430d92aed8db73413ad56dfff
Rubén Segarra <Rubn Segarra>
2015-11-26 19:02:43 +0100

    created project

---
+++ KataRubenToni/src/kata/Main.java
@@ -0,0 +1,11 @@
+package kata;
+
+/**
+ * Created by Ruben on 26/11/2015.
+ */
+public class Main {
+
+    public static void main(String args[]) {
+        System.out.print("Hello World");
+    }
+}
