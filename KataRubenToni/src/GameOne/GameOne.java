b680f7a8280ce5999b9e71f649d52b54738a664c
Antonio López Marín <tonilopezmr@gmail.com>
2015-11-26 19:20:21 +0100

    Created game two and three skeleton and Game interface

--- KataRubenToni/src/GameOne/GameOne.java
+++ KataRubenToni/src/GameOne/GameOne.java
@@ -4,4 +4,5 @@ package GameOne;
  * Created by Ruben on 26/11/2015.
  */
 public class GameOne {
+
 }
---
+++ KataRubenToni/src/kata/Game.java
@@ -0,0 +1,9 @@
+package kata;
+
+/**
+ * Created by toni on 26/11/15.
+ */
+public interface Game {
+    void play();
+    void exit();
+}
