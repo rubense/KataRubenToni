84f0883aecce5ecc95434e4dcf97772cb98ae4ad
Rubén Segarra <Rubn Segarra>
2015-11-29 18:35:10 +0100

    Game colors uses NumberValidator

--- GameOne/GameColors.java
+++ GameOne/GameColors.java
@@ -22,33 +22,38 @@ public class GameColors implements Game {
     }
 
     public void loadColors() {
-            this.colors.add(new Color("Rojo"));
-            this.colors.add(new Color("Verde"));
-            this.colors.add(new Color("Azul"));
-            this.colors.add(new Color("Amarillo"));
+        this.colors.add(new Color("Rojo"));
+        this.colors.add(new Color("Verde"));
+        this.colors.add(new Color("Azul"));
+        this.colors.add(new Color("Amarillo"));
     }
 
     public void game() {
-        boolean correct = false;
-        for (int i = 0; i < colors.size(); i++) {
-            System.out.println(i + ". " + colors.get(i).toString());
-        }
-        while (!correct) {
-            System.out.print("Guess the color (choose a number) : ");
-            Scanner sc1 = new Scanner(System.in);
-            //ValidatorManager.valid(numberValidator, "Please, put a number :D", sc1);
-            int numColor = sc1.nextInt();
-            int numRandom = (int) (Math.random() * colors.size());
-            if (numColor == numRandom) correct = true;
-            if (!correct) System.out.println("Error :( Try again");
-            else {
-                System.out.println("Great! :) You guess the color");
-                System.out.print("¿Try again? y/n ");
-                Scanner sc2 = new Scanner(System.in);
-                if (sc2.nextLine().equals("y")) correct = false;
+        try {
+            boolean correct = false;
+            for (int i = 0; i < colors.size(); i++) {
+                System.out.println(i + ". " + colors.get(i).toString());
             }
+            while (!correct) {
+                System.out.print("Guess the color (choose a number) : ");
+                Scanner sc1 = new Scanner(System.in);
+                numberValidator = (NumberValidator) ValidatorManager.valid(numberValidator, "Please, put a number :D", sc1);
+                int numColor = numberValidator.getValid();
+                int numRandom = (int) (Math.random() * colors.size());
+                if (numColor == numRandom) correct = true;
+                if (!correct) System.out.println("Error :( Try again");
+                else {
+                    System.out.println("Great! :) You guess the color");
+                    System.out.print("¿Try again? y/n ");
+                    Scanner sc2 = new Scanner(System.in);
+                    if (sc2.nextLine().equals("y")) correct = false;
+                }
+            }
+        } catch (NumberValidator.BetterCallFirstIsValidMethodException e) {
+            e.printStackTrace();
         }
     }
+    
 
     @Override
     public void play() {
